536fe4473dd52c21e976843fd8d2f1c924aa92b4
Neluttu <neluttu@gmail.com>
2023-10-04 17:26:52 +0300

    First page done

---
+++ main.js
@@ -0,0 +1,66 @@
+const titles = [
+    {
+        id: 1,
+        category: 'FANTASY/SCIENCE FICTION',
+        title: 'Arrival',
+        score: '8.4',
+        image: 'https://m.media-amazon.com/images/M/MV5BMTExMzU0ODcxNDheQTJeQWpwZ15BbWU4MDE1OTI4MzAy._V1_QL75_UX190_CR0,8,190,281_.jpg',
+    },
+    {
+        id: 2,
+        category: 'FANTASY/SCIENCE FICTION',
+        title: 'The 5th Wave',
+        score: '5.0',
+        image: 'https://m.media-amazon.com/images/M/MV5BMjQwOTc0Mzg3Nl5BMl5BanBnXkFtZTgwOTg3NjI2NzE@._V1_QL75_UX190_CR0,0,190,281_.jpg',
+    },
+    {
+        id: 3,
+        category: 'FANTASY/SCIENCE FICTION',
+        title: 'Captain America: Civil War',
+        score: '7.8',
+        image: 'https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_QL75_UX190_CR0,0,190,281_.jpg',
+    },
+    {
+        id: 4,
+        category: 'COMEDY',
+        title: 'Zoolander 2',
+        score: '4.8',
+        image: 'https://m.media-amazon.com/images/M/MV5BNTdkMzY4MjItNGM4My00ZDM1LThhMmQtY2NmMTYxZWM2ZDBmXkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_QL75_UX190_CR0,9,190,281_.jpg',
+    },
+    {
+        id: 5,
+        category: 'COMEDY',
+        title: 'Keanu',
+        score: '6.3',
+        image: 'https://m.media-amazon.com/images/M/MV5BMjBlOTIxNGMtMDAwOS00OTFjLWJkNWEtMDg5NGU0NzE5ZWRhXkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_QL75_UX190_CR0,2,190,281_.jpg',
+    },
+    {
+        id: 6,
+        category: 'COMEDY',
+        title: 'The Nice Guys',
+        score: '6.3',
+        image: 'https://m.media-amazon.com/images/M/MV5BODNlNmU4MGItMzQwZi00NGQyLWEyZWItYjFkNmI0NWI4NjBhXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_QL75_UX190_CR0,0,190,281_.jpg',
+    },
+    {
+        id: 7,
+        category: 'ROMANCE/DRAMA',
+        title: 'My Big Fat Greek Wedding 3',
+        score: '5.3',
+        image: 'https://m.media-amazon.com/images/M/MV5BMDcyZTJmNmEtYzAyMS00MWZmLWFhZmItMmM2MTFiMjQyOTgwXkEyXkFqcGdeQXVyMTM1NjM2ODg1._V1_QL75_UX190_CR0,0,190,281_.jpg',
+    },
+    {
+        id: 8,
+        category: 'ROMANCE/DRAMA',
+        title: 'Love Is In The Air',
+        score: '5.6',
+        image: 'https://m.media-amazon.com/images/M/MV5BMjI2ODEyYWUtM2ZkYy00YmIwLWI3ZDAtOWE5ZTZhZWZkZDZkXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_QL75_UX190_CR0,0,190,281_.jpg',
+    },
+    {
+        id: 9,
+        category: 'ROMANCE/DRAMA',
+        title: 'Past Lives',
+        score: '8.0',
+        image: 'https://m.media-amazon.com/images/M/MV5BOTkzYmMxNTItZDAxNC00NGM0LWIyODMtMWYzMzRkMjIyMTE1XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_QL75_UX190_CR0,0,190,281_.jpg',
+    },
+]
+
